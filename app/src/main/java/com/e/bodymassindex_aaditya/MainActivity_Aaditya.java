107dbad55db5115df20836f091ce60dd509a5d75
Aaditya Upreti <Aaditya Upreti>
2019-04-13 00:01:08 +0545

    Initial commit

---
+++ app/src/main/java/com/e/bodymassindex_aaditya/MainActivity_Aaditya.java
@@ -0,0 +1,73 @@
+package com.e.bodymassindex_aaditya;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity_Aaditya extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnCalculate;
+    private EditText etHeight, etWeight;
+    private TextView etOutput;
+
+    float height, weight, res;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_aaditya);
+
+        btnCalculate = findViewById(R.id.btnCalc);
+        etHeight = findViewById(R.id.etHeight);
+        etWeight= findViewById(R.id.etWeight);
+        etOutput = findViewById(R.id.etOutput);
+
+        btnCalculate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnCalc) {
+            if (validation()) {
+                height = Float.parseFloat(etHeight.getText().toString());
+                weight = Float.parseFloat(etWeight.getText().toString());
+                BMI_Aaditya obj = new BMI_Aaditya();
+                obj.setHeight(height);
+                obj.setWeight(weight);
+                res = obj.calcBMI();
+                etOutput.setText("Your BMI is " + Float.toString(res));
+
+                if (res < 18.5) {
+                    Toast.makeText(MainActivity_Aaditya.this, "Under Weight", Toast.LENGTH_LONG).show();
+                } else if (res >= 18.5 && res <= 24.9) {
+                    Toast.makeText(MainActivity_Aaditya.this, "Normal Weight", Toast.LENGTH_LONG).show();
+                } else if (res >= 25 && res <= 29.9) {
+                    Toast.makeText(MainActivity_Aaditya.this, "Over Weight", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity_Aaditya.this, "Obesity", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+    }
+
+    public boolean validation(){
+        boolean flag = true;
+
+        if (TextUtils.isEmpty(etHeight.getText().toString())){
+            etHeight.setError("Please enter Height");
+            etHeight.requestFocus();
+            flag = false;
+        }
+        else if (TextUtils.isEmpty(etWeight.getText().toString())){
+            etWeight.setError("Please enter Height");
+            etWeight.requestFocus();
+            flag = false;
+        }
+        return flag;
+    }
+}
